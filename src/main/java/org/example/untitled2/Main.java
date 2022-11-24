413c3869604c84e155ab543261881ddd4e96c0d3
Konstantin Kharun <konstantin19980209@gmail.com>
2022-11-24 15:31:46 +0300

    work

---
+++ src/main/java/org/example/untitled2/Main.java
@@ -0,0 +1,8 @@
+package org.example.untitled2;
+
+public class Main {
+        public static void main(String[] args) {
+            SQRService service = new SQRService();
+            System.out.println(service.calcService(200, 300));
+        }
+}
---
+++ src/main/java/org/example/untitled2/SQRService.java
@@ -0,0 +1,13 @@
+package org.example.untitled2;
+
+public class SQRService {
+    public int calcService(int x, int y) {
+        int result = 0;
+        for (int i = 10; i < 100; i++) {
+            if ((i * i >= x) && (i * i <= y)) {
+                result++;
+            }
+        }
+        return result;
+    }
+}
